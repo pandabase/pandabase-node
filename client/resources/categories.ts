5bc90fa90dcbcef74bceb1a96647e553d5d91ff5
Aelpxy <aelpxy@protonmail.com>
2024-10-22 14:03:50 +0600

    feat: categories added

--- client/resources/categories.ts
+++ client/resources/categories.ts
@@ -2,17 +2,67 @@ import { AxiosInstance } from "axios";
 
 import { ApiResponse, PaginationQuerystring } from "../../types/common";
 
+import {
+  CreateCategoryRequestBody,
+  CreateCategoryResponse,
+  DeleteCategoryResponse,
+  ListCategoryResponse,
+  RetrieveCategoryResponse,
+  UpdateCategoryRequestBody,
+  UpdateCategoryResponse,
+} from "../../types/resources/categories";
+
 import { formatUrlParams } from "../utils/formatURLParam";
 
 export function categoryOperations(api: AxiosInstance, shopId: string) {
   const baseUrl = `/shops/${shopId}/categories`;
 
   return {
-    list: async (querystring?: PaginationQuerystring) => {},
-    retrieve: async (id: string) => {},
-    retrieveByHandle: async (handle: string) => {},
-    create: async () => {},
-    update: async (id: string) => {},
-    delete: async (id: string) => {},
+    list: async (querystring?: PaginationQuerystring) => {
+      const url = formatUrlParams(baseUrl, {
+        page: querystring?.page,
+        page_size: querystring?.page_size,
+      });
+
+      const response = await api.get<ApiResponse<ListCategoryResponse>>(url);
+      return response.data.payload;
+    },
+    retrieve: async (id: string) => {
+      const response = await api.get<ApiResponse<RetrieveCategoryResponse>>(
+        `${baseUrl}/${id}`
+      );
+
+      return response.data.payload;
+    },
+    retrieveByHandle: async (handle: string) => {
+      const response = await api.get<ApiResponse<RetrieveCategoryResponse>>(
+        `${baseUrl}/slug/${handle}`
+      );
+
+      return response.data.payload;
+    },
+    create: async (body: CreateCategoryRequestBody) => {
+      const response = await api.post<ApiResponse<CreateCategoryResponse>>(
+        baseUrl,
+        body
+      );
+
+      return response.data.payload;
+    },
+    update: async (id: string, body: UpdateCategoryRequestBody) => {
+      const response = await api.patch<ApiResponse<UpdateCategoryResponse>>(
+        baseUrl,
+        body
+      );
+
+      return response.data.payload;
+    },
+    delete: async (id: string) => {
+      const response = await api.delete<ApiResponse<DeleteCategoryResponse>>(
+        baseUrl + "/" + id
+      );
+
+      return response.data.payload;
+    },
   };
 }
--- types/resources/categories.ts
+++ types/resources/categories.ts
@@ -2,27 +2,38 @@ import { PaginatedResponse } from "../common";
 
 // BASE
 
-type BaseCategoryData = {};
+type BaseCategoryData = {
+  id: string;
+  name: string;
+  handle: string;
+  created_at: string;
+  updated_at: string;
+};
 
 // REQUEST
 
-export interface CreateCategoryRequestBody {}
+export interface CreateCategoryRequestBody {
+  name: string;
+  handle: string;
+  products?: string[];
+}
 
-export interface UpdateCategoryRequestBody {}
+export interface UpdateCategoryRequestBody
+  extends Partial<CreateCategoryRequestBody> {}
 
 // RESPONSES
 
-export type ListProductResponse = PaginatedResponse<
+export type ListCategoryResponse = PaginatedResponse<
   "categories",
   BaseCategoryData[]
 >;
 
-export type RetrieveProductResponse = {
-  product: BaseCategoryData;
+export type RetrieveCategoryResponse = {
+  category: BaseCategoryData;
 };
 
-export type CreateCategoryResponse = { product: BaseCategoryData };
+export type CreateCategoryResponse = { category: BaseCategoryData };
 
-export type UpdateCategoryResponse = { product: BaseCategoryData };
+export type UpdateCategoryResponse = { category: BaseCategoryData };
 
-export type DeleteCategoryResponse = { product: { id: string } };
+export type DeleteCategoryResponse = { category: { id: string } };
